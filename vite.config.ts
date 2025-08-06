d22cd8b45cc00ee4e204387b8cd7d947b72301c5
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-07 02:41:05 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+});
