0525430e41644f214ce50e7f6eed2182e2748534
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-19 16:11:42 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ import vitePluginSvgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths(), vitePluginSvgr()],
-  base: "./",
+  base: "/",
 });
