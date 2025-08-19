a36b694da5dccf3b5e8b2a0d3e56090e20927b50
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-19 12:25:32 +0300

    fix

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -2,7 +2,7 @@ import Arrow from "@icons/arrow.svg?react";
 import Facbook from "@icons/facebook.svg?react";
 import Instagram from "@icons/instagram.svg?react";
 import Twitter from "@icons/twitter.svg?react";
-import Menu from "@icons/menu.svg";
+import Menu from "@icons/Menu.svg";
 import Star from "@icons/star.svg?react";
 import Cart from "@icons/cart.svg?react";
 import Phone from "@icons/phone.svg?react";
