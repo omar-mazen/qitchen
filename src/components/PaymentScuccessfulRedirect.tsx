bd62e4f21d19f19cdad8ed8e452b4fc8dd30499d
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-20 10:17:28 +0300

    fix payment

--- src/components/PaymentScuccessfulRedirect.tsx
+++ src/components/PaymentScuccessfulRedirect.tsx
@@ -1,15 +1,11 @@
 import { Navigate, useSearchParams } from "react-router";
-import { useQuery } from "@tanstack/react-query";
 
 import { ROUTES } from "@constants/routes";
-import { verifyPament } from "@services/order";
-import Loader from "./Loader";
-import { useCart } from "@/context/CartContext";
-import { useEffect } from "react";
 
 const PaymentScuccessfulRedirect = () => {
   const [searchParams] = useSearchParams();
   const orderId = searchParams.get("order_id");
+  console.log(window.location);
   if (orderId) return <Navigate to={`${ROUTES.ORDERS}/${orderId}`} />;
 };
 
