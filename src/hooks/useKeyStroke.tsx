684ab3a4671a0a3d73a1f4c10cc471a1cb860c12
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2025-08-20 10:25:15 +0300

    fix type in useKeyStroke

--- src/hooks/useKeyStroke.tsx
+++ src/hooks/useKeyStroke.tsx
@@ -6,12 +6,12 @@ export default function useKeystroke(
   targetElement: Document | HTMLElement = document
 ) {
   useEffect(() => {
-    function handleKeydown(e: KeyboardEvent) {
-      if (e.key === key) {
+    function handleKeydown(e: Event) {
+      const keyboardEvent = e as KeyboardEvent;
+      if (keyboardEvent.key === key) {
         handler();
       }
     }
-
     targetElement.addEventListener("keydown", handleKeydown);
     return () => {
       targetElement.removeEventListener("keydown", handleKeydown);
